1efdfb6aa3525051024f19b003eb3a6410271781
Alex Gibson <a.gibson@me.com>
2014-08-06 21:02:17 +0100

    Move build files into src directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ var minifyCSS = require('gulp-minify-css');
 
 gulp.task('default', function() {
     gulp.src([
-        './js/jquery-min.js',
-        './js/underscore-min.js',
-        './js/backbone-min.js',
-        './js/backbone.localstorage-min.js',
-        './js/tap.js',
-        './js/main.js'
+        './src/js/jquery-min.js',
+        './src/js/underscore-min.js',
+        './src/js/backbone-min.js',
+        './src/js/backbone.localstorage-min.js',
+        './src/js/tap.js',
+        './src/js/main.js'
         ])
         .pipe(concat('app.js'))
         .pipe(uglify({'preserveComments': 'all'}))
         .pipe(gulp.dest('./dist/js/'));
 
     gulp.src([
-        './css/normalize.css',
-        './css/styles.css'
+        './src/css/normalize.css',
+        './src/css/styles.css'
         ])
         .pipe(concat('styles.css'))
         .pipe(minifyCSS())
